c3bafcb99bc1eded13755f270e5b3a31324dbbfb
lcoombe <lauren.e.coombe@gmail.com>
2019-06-26 10:43:40 -0700

    Update tests
    * Launch full test using Makefile

--- tests/minscaff_test.py
+++ tests/minscaff_test.py
@@ -3,11 +3,11 @@ import subprocess
 import re
 
 def test_mx():
-    cmd = "../minimizer_assemble.py -p test_min -n 1 -g 50 -l \"1 1\" -k 32 " \
-          "ref.fa.k32.w1000.tsv scaf.fa.k32.w1000.tsv"
+    cmd = "../minimizer_assembler-make assemble list_files=\'ref.fa scaf.fa\' " \
+          "list_weights=\'2 1\' k=32 w=1000 n=2 prefix=out_scaf.k32.w1000.n2"
     cmd_shlex = shlex.split(cmd)
     subprocess.call(cmd_shlex)
-    with open("test_min.path", 'r') as paths:
+    with open("out_scaf.k32.w1000.n2.path", 'r') as paths:
         for line in paths:
             path_match = re.search(r'^mx', line)
             if path_match:
@@ -15,11 +15,11 @@ def test_mx():
 
 
 def test_mx_rc():
-    cmd = "../minimizer_assemble.py -p test_min_rc -n 1 -g 50 -l \"1 1\" -k 32 " \
-          "ref.fa.k32.w1000.tsv scaf.rc.fa.k32.w1000.tsv"
+    cmd = "../minimizer_assembler-make assemble list_files=\'ref.fa scaf.rc.fa\' " \
+          "list_weights=\'2 1\' k=32 w=1000 n=2 prefix=out_scaf.rc.k32.w1000.n2"
     cmd_shlex = shlex.split(cmd)
     subprocess.call(cmd_shlex)
-    with open("test_min_rc.path", 'r') as paths:
+    with open("out_scaf.rc.k32.w1000.n2.path", 'r') as paths:
         for line in paths:
             path_match = re.search(r'^mx', line)
             if path_match:
