71386bcbffab0045a037c9adcf36cc8756932c63
lcoombe <lauren.e.coombe@gmail.com>
2019-07-09 15:28:01 -0700

    Fix bedtools installation in .yml, check return code for subprocess call

--- tests/minscaff_test.py
+++ tests/minscaff_test.py
@@ -8,7 +8,8 @@ def test_mx():
     cmd = "../minimizer_assembler-make assemble -B list_files=\'ref.fa scaf.fa\' " \
           "list_weights=\'2 1\' k=32 w=1000 n=2 prefix=out_scaf.k32.w1000.n2"
     cmd_shlex = shlex.split(cmd)
-    subprocess.call(cmd_shlex)
+    return_code = subprocess.call(cmd_shlex)
+    assert return_code == 0
     with open("out_scaf.k32.w1000.n2.path", 'r') as paths:
         for line in paths:
             path_match = re.search(r'^mx', line)
@@ -21,7 +22,8 @@ def test_mx_rc():
     cmd = "../minimizer_assembler-make assemble -B list_files=\'ref.fa scaf.rc.fa\' " \
           "list_weights=\'2 1\' k=32 w=1000 n=2 prefix=out_scaf.rc.k32.w1000.n2"
     cmd_shlex = shlex.split(cmd)
-    subprocess.call(cmd_shlex)
+    return_code = subprocess.call(cmd_shlex)
+    assert return_code == 0
     with open("out_scaf.rc.k32.w1000.n2.path", 'r') as paths:
         for line in paths:
             print(line)
