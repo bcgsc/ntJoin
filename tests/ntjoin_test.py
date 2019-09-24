96ab62ba9375384c23b43249c792dd9f303178f4
lcoombe <lauren.e.coombe@gmail.com>
2019-09-24 11:53:37 -0700

    ntjoin_test.py: pylint fixes

--- tests/ntjoin_test.py
+++ tests/ntjoin_test.py
@@ -1,8 +1,12 @@
+"""Tests for ntJoin"""
+
 import shlex
 import subprocess
 import re
 
-def run_ntJoin(file1, file2, prefix, window=1000, n=2):
+
+def run_ntjoin(file1, file2, prefix, window=1000, n=2):
+    "Run ntJoin with a pair of files"
     cmd = "../ntJoin assemble -B target=%s target_weight=1 references=\'%s\' reference_weights=\'2\' " \
           "prefix=%s k=32 w=%s n=%s" % (file2, file1, prefix, window, n)
     cmd_shlex = shlex.split(cmd)
@@ -16,7 +20,8 @@ def run_ntJoin(file1, file2, prefix, window=1000, n=2):
                 return_paths.append(line.strip())
     return return_paths
 
-def run_ntJoin_multiple(file1, file2, file3, prefix, window=1000, n=2):
+def run_ntjoin_multiple(file1, file2, file3, prefix, window=1000, n=2):
+    "Run ntJoin with a target and 2 references"
     cmd = "../ntJoin assemble -B target=%s target_weight=1 references=\'%s %s\' reference_weights=\'2 2\' " \
           "prefix=%s k=32 w=%s n=%s" % (file3, file1, file2, prefix, window, n)
     cmd_shlex = shlex.split(cmd)
@@ -30,40 +35,42 @@ def run_ntJoin_multiple(file1, file2, file3, prefix, window=1000, n=2):
                 return_paths.append(line.strip())
     return return_paths
 
+# Following 4 tests to check for the expected PATHs given 2 pieces that should be merged
+#     together based on the reference in different orientations
+#     - Pieces are the reference piece split, with ~20bp in between
 
-'''Following 4 tests to check for the expected PATHs given 2 pieces that should be merged
-    together based on the reference in different orientations
-    - Pieces are the reference piece split, with ~20bp in between
-'''
 def test_mx_f_f():
-    paths = run_ntJoin("ref.fa", "scaf.f-f.fa", "f-f_test")
+    "Testing ntJoin with assembly + reference, fwd-fwd orientation"
+    paths = run_ntjoin("ref.fa", "scaf.f-f.fa", "f-f_test")
     assert len(paths) == 1
     assert paths.pop() == "ntJoin0\t1_f+:0-1981 20N 2_f+:0-2329"
 
 
 def test_mx_f_r():
-    paths = run_ntJoin("ref.fa", "scaf.f-r.fa", "f-r_test")
+    "Testing ntJoin with assembly + reference, fwd-rev orientation"
+    paths = run_ntjoin("ref.fa", "scaf.f-r.fa", "f-r_test")
     assert len(paths) == 1
     assert paths.pop() == "ntJoin0\t1_f+:0-1981 20N 2_r-:0-2329"
 
 
 def test_mx_r_f():
-    paths = run_ntJoin("ref.fa", "scaf.r-f.fa", "r-f_test")
+    "Testing ntJoin with assembly + reference, rev-fwd orientation"
+    paths = run_ntjoin("ref.fa", "scaf.r-f.fa", "r-f_test")
     assert len(paths) == 1
     assert paths.pop() == "ntJoin0\t1_r-:0-1981 20N 2_f+:0-2329"
 
 
 def test_mx_r_r():
-    paths = run_ntJoin("ref.fa", "scaf.r-r.fa", "r-r_test")
+    "Testing ntJoin with assembly + reference, rev-rev orientation"
+    paths = run_ntjoin("ref.fa", "scaf.r-r.fa", "r-r_test")
     assert len(paths) == 1
     assert paths.pop() == "ntJoin0\t1_r-:0-1981 20N 2_r-:0-2329"
 
-'''
-Test checks for the expected gap length and sequence orientation for a 
-test with 2 expected output paths
-'''
+# Test checks for the expected gap length and sequence orientation for a
+# test with 2 expected output paths
 def test_gap_dist_multiple():
-    paths = run_ntJoin("ref.multiple.fa", "scaf.multiple.fa", "gap-dist_test", window=500, n=1)
+    "Testing ntJoin with assembly + reference, estimated gap length"
+    paths = run_ntjoin("ref.multiple.fa", "scaf.multiple.fa", "gap-dist_test", window=500, n=1)
     assert len(paths) == 2
     assert paths[0] != paths[1]
     expected_paths = ["2_1_p+:0-2492 100N 2_2_n-:0-2574", "1_1_p+:0-1744 124N 1_2_p+:0-1844"]
@@ -71,13 +78,12 @@ def test_gap_dist_multiple():
     assert paths.pop().split("\t")[1] in expected_paths
 
 
-'''
-Tests for gap distance estimation, misassembled scaffolds
-Expected that 2 input scaffolds will be broken and joined based on the reference.
-Testing orientations of joins: +/+ -/- +/- -/+
-'''
+# Tests for gap distance estimation, misassembled scaffolds
+# Expected that 2 input scaffolds will be broken and joined based on the reference.
+# Testing orientations of joins: +/+ -/- +/- -/+
 def test_regions_ff_rr():
-    paths = run_ntJoin("ref.multiple.fa", "scaf.misassembled.f-f.r-r.fa", "regions-ff-rr_test", window=500, n=1)
+    "Testing ntJoin correcting misassemblies, joins in fwd-fwd and rev-rev"
+    paths = run_ntjoin("ref.multiple.fa", "scaf.misassembled.f-f.r-r.fa", "regions-ff-rr_test", window=500, n=1)
     assert len(paths) == 2
     assert paths[0] != paths[1]
     expected_paths = ["2_1n-1_2p-:0-2176 20N 1_1p-2_2n-:2010-4489", "1_1p-2_2n+:0-1541 468N 2_1n-1_2p+:2676-4379"]
@@ -86,7 +92,8 @@ def test_regions_ff_rr():
 
 
 def test_regions_fr_rf():
-    paths = run_ntJoin("ref.multiple.fa", "scaf.misassembled.f-r.r-f.fa", "regions-fr-rf_test", 500, n=2)
+    "Testing ntJoin correcting misassemblies, joins in fwd-rev and rev-fwd"
+    paths = run_ntjoin("ref.multiple.fa", "scaf.misassembled.f-r.r-f.fa", "regions-fr-rf_test", 500, n=2)
     assert len(paths) == 2
     assert paths[0] != paths[1]
     expected_paths = ["2_1n-1_2n-:0-2176 212N 1_1p-2_2p+:2017-4489", "1_1p-2_2p+:0-1617 198N 2_1n-1_2n-:2675-4379"]
@@ -94,6 +101,8 @@ def test_regions_fr_rf():
     assert paths.pop().split("\t")[1] in expected_paths
 
 def test_regions_3():
-    paths = run_ntJoin_multiple("ref.fa", "scaf.f-f.copy.fa", "scaf.f-f.fa", "f-f-f_test", n=1)
+    "Testing ntJoin with target + 2 references"
+    paths = run_ntjoin_multiple("ref.fa", "scaf.f-f.copy.fa", "scaf.f-f.fa", "f-f-f_test", n=1)
     assert len(paths) == 1
     assert paths.pop() == "ntJoin0\t1_f+:0-1981 20N 2_f+:0-2329"
+    
