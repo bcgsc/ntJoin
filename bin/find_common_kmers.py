4e8357f16226ff12965d1f530031c53cbeb204f4
lcoombe <lauren.e.coombe@gmail.com>
2020-04-17 16:26:44 -0700

    find_common_kmers.py: Use for finding common kmers instead of joining files

---
+++ bin/find_common_kmers.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+Scans through KMC output of 2 files, prints out kmers in common between the two
+"""
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Find common kmers between KMC output")
+    parser.add_argument("-t", help="Target KMC file", required=True)
+    parser.add_argument("-r", help="Reference KMC file", required=True)
+    args = parser.parse_args()
+
+    ref_kmers = set()
+
+    with open(args.r, 'r') as ref_kmers_file:
+        for kmer in ref_kmers_file:
+            kmer, count = kmer.strip().split("\t")
+            ref_kmers.add(kmer)
+
+    with open(args.t, 'r') as target_kmers_file:
+        for kmer in target_kmers_file:
+            kmer, count = kmer.strip().split("\t")
+            if kmer in ref_kmers:
+                print(kmer, count, sep="\t")
+
+
+if __name__ == "__main__":
+    main()
