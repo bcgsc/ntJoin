1f23a1fd10ff8df23bd3b908a1fdc5d8b92505bf
lcoombe <lauren.e.coombe@gmail.com>
2020-04-17 16:30:55 -0700

    Fixes for pylint

--- bin/find_common_kmers.py
+++ bin/find_common_kmers.py
@@ -7,6 +7,7 @@ Scans through KMC output of 2 files, prints out kmers in common between the two
 import argparse
 
 def main():
+    "Only print kmers in common between the KMC input files"
     parser = argparse.ArgumentParser(description="Find common kmers between KMC output")
     parser.add_argument("-t", help="Target KMC file", required=True)
     parser.add_argument("-r", help="Reference KMC file", required=True)
