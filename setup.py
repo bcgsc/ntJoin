c245399cea1767aa2462aa0a26bb7bddd614a5ee
lcoombe <lauren.e.coombe@gmail.com>
2023-09-08 08:34:01 -0700

    Prepare for release v1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ntJoin",
-    version="1.1.1",
+    version="1.1.2",
     author="Lauren Coombe",
     author_email="lcoombe@bcgsc.ca",
     description="Genome assembly scaffolder using minimizer graphs",
